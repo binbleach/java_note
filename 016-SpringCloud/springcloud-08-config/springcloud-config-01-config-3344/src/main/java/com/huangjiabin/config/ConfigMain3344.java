a702f9807f3b770cc6e275f45ef05a5558ec3337
hjbin <2048622419@qq.com>
2023-03-06 15:16:45 +0800

    1

--- 016-SpringCloud/springcloud-08-config/springcloud-config-01-config-3344/src/main/java/com/huangjiabin/config/ConfigMain3344.java
+++ 016-SpringCloud/springcloud-08-config/springcloud-config-01-config-3344/src/main/java/com/huangjiabin/config/ConfigMain3344.java
@@ -1,16 +1,22 @@
 package com.huangjiabin.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
     内容讲解：
     一、config知识卡片：
         SpringCloud Config为微服务架构中的微服务提供集中化的外部配置支持，配置服务器为各人不同微服务应用的
         所有环境提供了一个中心化的外部配置。
- 
+
  */
-public class App
+@SpringBootApplication
+@EnableConfigServer     //激活配置中心
+public class ConfigMain3344
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(ConfigMain3344.class,args);
     }
 }
